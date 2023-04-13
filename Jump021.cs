3a14d90b87b7af9e46f83b35e7d0609088e01c07
flower777777777 <95156497+flower777777777@users.noreply.github.com>
2023-04-13 11:25:05 +0800

    Add files via upload

---
+++ Jump021.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Jump021 : MonoBehaviour {
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
---
+++ Jump122.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Jump122 : MonoBehaviour {
+	public void Onclick()
+	{
+		SceneManager.LoadScene("Scene2");
+	}
+}
