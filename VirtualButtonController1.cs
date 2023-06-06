75a62804f28b74e1f78b2141f45fc37ce70a1590
flower777777777 <95156497+flower777777777@users.noreply.github.com>
2023-06-06 21:49:56 +0800

    Add files via upload

---
+++ VirtualButtonController1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualButtonController1 : MonoBehaviour ,IVirtualButtonEventHandler{
+
+	public GameObject Right_1;
+	public GameObject Wrong_1;
+	// Use this for initialization
+	void Start () {
+		VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+		for(int i = 0; i < vbs.Length; i++)
+		{
+			vbs[i].RegisterEventHandler(this);
+		}
+		Right_1.SetActive(false);
+		Wrong_1.SetActive(false);
+	}
+	public void OnButtonPressed(VirtualButtonBehaviour vb)
+	{
+		switch (vb.VirtualButtonName)
+		{
+			case "VirtualButton1":
+				Right_1.SetActive(true);
+				break;
+			case "VirtualButton2":
+				Wrong_1.SetActive(true);
+				break;
+			case "VirtualButton3":
+				Wrong_1.SetActive(true);
+				break;
+			case "VirtualButton4":
+				Right_1.SetActive(true);
+				break;
+		}
+	}
+	public void OnButtonReleased(VirtualButtonBehaviour vb)
+	{
+		switch (vb.VirtualButtonName)
+		{
+			case "VirtualButton1":
+				Right_1.SetActive(false);
+				break;
+			case "VirtualButton2":
+				Wrong_1.SetActive(false);
+				break;
+			case "VirtualButton3":
+				Wrong_1.SetActive(false);
+				break;
+			case "VirtualButton4":
+				Right_1.SetActive(false);
+				break;
+			
+		}
+	}
+	
+	
+}
---
+++ VirtualButtonController2.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualButtonController2 : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject Right_1;
+    public GameObject Wrong_1;
+    // Use this for initialization
+    void Start () {
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; i++)
+        {
+            vbs[i].RegisterEventHandler(this);
+        }
+        Right_1.SetActive(false);
+        Wrong_1.SetActive(false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        switch (vb.VirtualButtonName)
+        {
+            /*case "VirtualButton5":
+                Right_1.SetActive(true);
+                break;
+            case "VirtualButton6":
+                Right_1.SetActive(true);
+                break;*/
+            case "VirtualButton7":
+                Right_1.SetActive(true);
+                break;
+            case "VirtualButton8":
+                Wrong_1.SetActive(true);
+                break;
+        }
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        switch (vb.VirtualButtonName)
+        {
+            /*case "VirtualButton5":
+                Right_1.SetActive(false);
+                break;
+            case "VirtualButton6":
+                Right_1.SetActive(false);
+                break;*/
+            case "VirtualButton7":
+                Right_1.SetActive(false);
+                break;
+            case "VirtualButton8":
+                Wrong_1.SetActive(false);
+                break;
+        }
+    }
+}
