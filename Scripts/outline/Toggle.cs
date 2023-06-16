8a56bc325fc2b37b27618f044c7b2fade67428fa
1544880115a <116902740+1544880115a@users.noreply.github.com>
2023-06-16 22:35:06 +0800

    Add files via upload

---
+++ Scripts/outline/Toggle.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace cakeslice
+{
+    public class Toggle : MonoBehaviour
+    {
+        private GameObject obj1;
+        private GameObject obj2;
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    obj1 = hitInfo.collider.gameObject;
+                    if (obj1.GetComponent<Outline>() != null)
+                    {
+                        if (obj2 != null && obj2.transform != obj1.transform && obj2.GetComponent<Rotate>() != null) obj2.GetComponent<Rotate>().enabled = true;
+                        if (obj1.GetComponent<Rotate>() != null) {
+                            obj1.GetComponent<Rotate>().enabled = false;
+                            obj2 = obj1;
+
+                        }
+                        else if (obj1.transform.parent.gameObject != null && obj1.transform.parent.gameObject.GetComponent<Rotate>() != null)
+                        {
+                            obj1.transform.parent.gameObject.GetComponent<Rotate>().enabled = false;
+                            obj2 = obj1.transform.parent.gameObject;
+                        }
+                        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+                        {
+                            if (Input.GetTouch(0).tapCount == 1)
+                            {
+                                Outline[] outlines = obj1.transform.parent.gameObject.GetComponentsInChildren<Outline>();
+                                for (int i = 0; i < outlines.Length; i++)
+                                {
+                                    outlines[i].enabled = !outlines[i].enabled;
+                                }
+                            }
+                        }
+                    }
+                    else if(obj1.GetComponent<SelectCenter>() != null)
+                    {
+                        GameObject.Find("gyroscope(Clone)").GetComponentInChildren<TouchRotate>().setCenter(obj1.transform.position);
+                        GameObject.Find("gyroscope(Clone)").GetComponentInChildren<TouchRotate>().setState(true);
+                        GameObject.Find("gyroscope(Clone)").GetComponentInChildren<TouchRotate>().setSpeed(1000);
+                    }
+                }
+                else if (!Physics.Raycast(ray, out hitInfo) && obj2 != null && obj2.GetComponent<Rotate>() != null)
+                {
+                    obj2.GetComponent<Rotate>().enabled = true;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Scripts/rotate/Enlarge.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+public class Enlarge : MonoBehaviour {
+
+    Vector2 oldPos1;
+    Vector2 oldPos2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount == 2 && this.GetComponentInChildren<Outline>().enabled == true)
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(1).phase == TouchPhase.Moved)
+            {
+                Vector2 temPos1 = Input.GetTouch(0).position;
+                Vector2 temPos2 = Input.GetTouch(1).position;
+
+                if(isEnLarge(oldPos1, oldPos2, temPos1, temPos2))
+                {
+                    float oldScalex = transform.localScale.x;
+                    float oldScaley = transform.localScale.y;
+                    float oldScalez = transform.localScale.z;
+
+                    transform.localScale = new Vector3(oldScalex * 1.025f, oldScaley, oldScalez * 1.025f);
+                }
+                else
+                {
+                    float oldScalex = transform.localScale.x;
+                    float oldScaley = transform.localScale.y;
+                    float oldScalez = transform.localScale.z;
+
+                    transform.localScale = new Vector3(oldScalex / 1.025f, oldScaley, oldScalez / 1.025f);
+                }
+
+                oldPos1 = temPos1;
+                oldPos2 = temPos2;
+            }
+        }
+	}
+
+    //判断手势
+    bool isEnLarge(Vector2 oP1, Vector2 oP2, Vector2 nP1, Vector2 nP2)
+    {
+        float length1 = Mathf.Sqrt((oP1.x - oP2.x) * (oP1.x - oP2.x) + (oP1.y - oP2.y) * (oP1.y - oP2.y));
+        float length2 = Mathf.Sqrt((nP1.x - nP2.x) * (nP1.x - nP2.x) + (nP1.y - nP2.y) * (nP1.y - nP2.y));
+
+        if(length1 < length2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
