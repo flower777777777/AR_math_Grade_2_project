7c78854802be66eb5fcd999eec1968fc4b26f7aa
1544880115a <116902740+1544880115a@users.noreply.github.com>
2023-06-16 23:12:55 +0800

    Add files via upload

---
+++ Assets/RotataAndSymmetryAssets/Scripts/rotate/Rotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+public class Rotate : MonoBehaviour {
+    float xSpeed = 150.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.GetComponentInChildren<Outline>().enabled == true)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (Input.touchCount == 1)
+                {
+                    if (Input.GetTouch(0).phase == TouchPhase.Moved)
+                    {
+                        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * -xSpeed * Time.deltaTime, Space.World);
+                    }
+                }
+            }
+        }
+	}
+}
---
+++ Assets/RotataAndSymmetryAssets/Scripts/rotate/Wiserotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiserotate : MonoBehaviour {
+    float xSpeed = 300.0f;
+    public GameObject obj;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.touchCount == 1)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Moved)
+                {
+                    transform.RotateAround(obj.transform.position, Vector3.up * Input.GetAxis("Mouse X"), -xSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+}
